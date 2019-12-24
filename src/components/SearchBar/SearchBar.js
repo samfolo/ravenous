ca2fa1bc63c42b633de2ff971b7d7673431b045f
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-12-24 16:53:37 +0000

    visual feedback on sortBy options, adding state to SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-  'Best Match': 'best_match',
-  'Highest Rated': 'rating',
-  'Most Reviewed': 'review_count'
-}
-
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
+
+    this.sortByOptions = {
+      'Best Match': 'best_match',
+      'Highest Rated': 'rating',
+      'Most Reviewed': 'review_count'
+    };
+  }
+
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      const sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOptionValue}</li>
+    return Object.keys(this.sortByOptions).map(sortByOption => {
+      const sortByOptionValue = this.sortByOptions[sortByOption];
+      return (
+        <li 
+          key={sortByOptionValue} 
+          className={this.getSortByClass(sortByOptionValue)}
+          onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+        >{sortByOption}</li>
+      )
     });
   }
+
+  getSortByClass(sortByOption) {
+    return this.state.sortBy === sortByOption ? 'active' : ''
+  };
+
+  handleSortByChange(sortByOption) {
+    this.setState({ sortBy: sortByOption });
+  };
 
   render() {
     return (
